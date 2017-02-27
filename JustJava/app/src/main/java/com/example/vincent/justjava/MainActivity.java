d1bcf5dedece28090b85ce7215217e703b52c888
vitus121289 <vmtusing@gmail.com>
2017-02-27 20:43:21 +0800

    -added a method that computes for the total price of the order assuming that the price per cup of coffee is $5.00. -and then displays that price under the order summary text view.

--- JustJava/app/src/main/java/com/example/vincent/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/vincent/justjava/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
@@ -32,11 +34,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * This method takes the order of the customer.
+     */
+    public void placeOrder(View v) {
+        int totalPrice = 0;
+        totalPrice = calculatePrice();
+        displayOrderSummary(totalPrice);
+    }
+
+    /**
+     * This method calculates the total price of the purchase.
+     *
+     * @return the total price of the purchase.
+     */
+    private int calculatePrice() {
+        int pricePerCup = 5;
+        return quantity * pricePerCup;
+    }
+
+    /**
      * This method displays the number of coffees the customer going to order.
+     *
      * @param quantity is the number of coffees the customer is going to order.
      */
     private void displayQuantity(int quantity) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + quantity);
+    }
+
+    /**
+     * This method displays the summary of the customer's order.
+     *
+     * @param price total price calculated by the method calculatePrice().
+     */
+    private void displayOrderSummary(int price) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText("$" + price);
     }
 }
